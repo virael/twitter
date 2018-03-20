ee0db9067e53db75fb4ecb1353af6c83490dd49e
lukas <lukas@lukas-system>
2018-03-20 14:05:36 +0100

    20.03.2018 14:05

---
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -0,0 +1,43 @@
+package pl.coderslab.app;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "pl.coderslab.controller", "pl.coderslab.entity" })
+@EnableJpaRepositories(basePackages= {"pl.coderslab.repository"})
+@EnableTransactionManagement
+public class AppConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Bean
+	public LocalEntityManagerFactoryBean entityManagerFactory() {
+		LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+		emfb.setPersistenceUnitName("warsztat6PersistenceUnit");
+		return emfb;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager tm = new JpaTransactionManager(emf);
+		return tm;
+	}
+}
